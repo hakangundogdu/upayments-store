071acd1b1cbb407efa5ddb2e8cc2b2a65215106d
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-07-17 16:49:44 +0300

    Nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,20 @@
+const Nav = () => {
+  return (
+    <header className="  flex justify-between h-16 mx-auto pb-4 border-b-2 border-slate-100">
+      <a href="/" className="flex items-center">
+        <img
+          src="https://upayments.com/en/wp-content/uploads/sites/4/2020/07/upay-logo.png"
+          alt="upay-logo"
+          className="w-auto h-6"
+        />
+        <p className="text-xl ml-4  text-gray-900">Store</p>
+      </a>
+
+      <button className="hover:text-slate-900  text-slate-600">
+        Register{' '}
+      </button>
+    </header>
+  );
+};
+
+export default Nav;

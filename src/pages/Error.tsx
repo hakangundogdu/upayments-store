33e07cce97fc72767b62bc2e19781edf20ec763b
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-07-19 12:04:31 +0300

    Error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Error = () => {
-  return <div>Not Found</div>;
+  return (
+    <div className="flex flex-col place-content-center mt-36 ">
+      <p className="font-bold text-2xl text-center text-slate-900">
+        Page not found
+      </p>
+      <button className="hover:text-slate-900 mt-12  text-slate-600">
+        <a href="/">Back to Homepage </a>
+      </button>
+    </div>
+  );
 };
 
 export default Error;
